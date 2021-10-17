3eb85db595b54ac71cd019413609efef0e11aaa5
delanosergeant <delano.sergeant@student.hogent.be>
2021-10-17 19:22:26 +0200

    Toevoegen Basis layout

--- app/src/main/java/com/example/kolveniershofapp/MainActivity.kt
+++ app/src/main/java/com/example/kolveniershofapp/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.kolveniershofapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
+    //private lateinit var mToolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //mToolbar = findViewById(R.id.main_toolbar)
+        //setSupportActionBar(mToolbar)
+
     }
+
+    @Override
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return true
+    }
+
+
 }
